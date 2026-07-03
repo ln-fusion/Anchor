70cbf476bde12ea719f5e55eddefcb97c2a9aa0a
taisiooc <taisiooc_1@163.com>
2026-07-04 00:46:01 +0800

    增加了按钮触发的逻辑（但是有bug）

---
+++ Anchor/Assets/Scripts/Map/Button&Door/ButtonTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("aaa");
+        //if (other.GetComponent<Hook>())
+        //{
+            door.SetActive(false);
+            //Debug.Log("yes");
+        //}
+    }
+}
