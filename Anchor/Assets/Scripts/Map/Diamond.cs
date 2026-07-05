471d539f01cc6297920a10fcac8d17c781fdf9c4
Non-night <99241021+Non-night@users.noreply.github.com>
2026-07-05 09:26:56 +0800

    实装level1美术，再调整单周目通关逻辑

--- Anchor/Assets/Scripts/Map/Diamond.cs
+++ Anchor/Assets/Scripts/Map/Diamond.cs
@@ -29,7 +29,7 @@ public class SwitchScene : MonoBehaviour
 
 	private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<Hook>())
+        if (other.GetComponent<Hook>() || other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             if (dialogSystem == null)
             {
